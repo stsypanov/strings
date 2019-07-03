f3fac78ff42a51f4d3e7ef12faef5c0c963761db
SergiiTsypanov <stsypanov@corelogic.com>
2019-07-03 10:51:24 +0300

    expose JMH-generated classes to runtime

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,9 @@ module strings.main {
   requires jmh.core;
   requires jdk.unsupported;
 
+  //bug in IDEA https://youtrack.jetbrains.com/issue/IDEA-211122, compilation is correct
   exports tsypanov.strings.benchmark.string.generated;
+  exports tsypanov.strings.benchmark.concatenation.generated;
+  exports tsypanov.strings.benchmark.reflection.generated;
+  exports tsypanov.strings.benchmark.p6spy.generated;
 }
