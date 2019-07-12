287cb2fd76cd023fffc0ebb0c7c835f3dc4a33a7
SergiiTsypanov <stsypanov@corelogic.com>
2019-07-12 17:43:03 +0300

    fix TornAppendBenchmark on JDK11

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module strings.main {
   requires jmh.core;
   requires jdk.unsupported;
+  requires org.apache.commons.lang3;
+  requires spring.core;
 
   //bug in IDEA https://youtrack.jetbrains.com/issue/IDEA-211122, compilation is correct
   exports tsypanov.strings.benchmark.string.generated;
