b6f540aa426650c28c3b42150c05f9ccaea0c71d
stsypanov <sergei.tsypanov@yandex.ru>
2020-07-01 14:47:00 +0300

    Add DeleteAnyBenchmark

---
+++ src/jmh/java/tsypanov/strings/string/DeleteAnyBenchmark.java
@@ -0,0 +1,49 @@
+package tsypanov.strings.string;
+
+
+/**
+ * @see https://github.com/spring-projects/spring-framework/pull/24870
+ */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(jvmArgsAppend = {"-Xms2g", "-Xmx2g", "-XX:+UseParallelGC"})
+public class DeleteAnyBenchmark {
+
+  private final String input = "key1=value1 ";
+  private final String replacement = "\"";
+
+  @Benchmark
+  public String original() {
+    return deleteAny(input, replacement);
+  }
+
+  @Benchmark
+  public String patched() {
+    return deleteAnyPatched(input, replacement);
+  }
+
+  private static String deleteAny(String inString, String charsToDelete) {
+    StringBuilder sb = new StringBuilder(inString.length());
+    for (int i = 0; i < inString.length(); i++) {
+      char c = inString.charAt(i);
+      if (charsToDelete.indexOf(c) == -1) {
+        sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
+
+  private static String deleteAnyPatched(String inString, String charsToDelete) {
+    int lastCharIndex = 0;
+    char[] result = new char[inString.length()];
+    for (int i = 0; i < inString.length(); i++) {
+      char c = inString.charAt(i);
+      if (charsToDelete.indexOf(c) == -1) {
+        result[lastCharIndex++] = c;
+      }
+    }
+    return new String(result, 0, lastCharIndex);
+  }
+
+}
